39ff4b36f058d1c6eaf5e8af014b60f41265d827
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2018-09-13 19:12:59 +0300

    add prompt

--- JS/hw-03/js/index.js
+++ JS/hw-03/js/index.js
@@ -2,33 +2,35 @@
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-const isLoginValid = function(login) {
+const login=prompt('Enter login');
+
+const isLoginValid = function(login){
   return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function(allLogins, login) {
+
   return !allLogins.includes(login);
 };
 
 const addLogin = function(logins, login) {
-  if (isLoginValid(login)) {
-    isLoginUnique(logins, login);
+    if (isLoginValid(login)) {
+    if (isLoginUnique(logins, login)) {
+      alert('Логин успешно добавлен!');
+      logins.push(login);
+      return logins;
+    } else {
+      alert('Такой логин уже используется!');
+      return logins;
+    }
   } else {
     alert('Ошибка! Логин должен быть от 4 до 16 символов');
     return logins;
   }
-  if (isLoginUnique(logins, login)) {
-    alert('Логин успешно добавлен!');
-    logins.push(login);
-    return logins;
-  } else {
-    alert('Такой логин уже используется!');
-    return logins;
-  }
 };
 
-//  addLogin (logins,'Ajax');
+ addLogin (logins,login);
 //  addLogin( logins,'robotGoogles')
 // addLogin( logins,'Zod');
 // addLogin(logins,'jqueryisextremelyfast')
-// console.log(logins);
+console.log(logins);
