cc09845a0b6f3fcda76501ac157a99a66152b061
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2018-11-27 21:32:34 +0200

    add err

--- JS/hw-10/js/index.js
+++ JS/hw-10/js/index.js
@@ -28,12 +28,12 @@ function showUser({ id, name, age }) {
 
 function getAllUser() {
   fetch('https://test-users-api.herokuapp.com/users')
-  .then(resp => {
-    if (resp.ok) return resp.json();
-    throw new Error(`${resp.statusText}`);
-  })
+    .then(resp => {
+      if (resp.ok) return resp.json();
+      throw new Error(`${resp.statusText}`);
+    })
     .then(data => createView(data.data))
-    .catch(err => console.log(err));
+    .catch(err => alert(`${err}`));
 }
 
 getAll.addEventListener('click', getAllUser);
@@ -48,7 +48,7 @@ function getUserById() {
       throw new Error(`${resp.statusText}`);
     })
     .then(data => showUser(data.data))
-    .catch(err => console.log(err));
+    .catch(err => alert(`${err}`));
 }
 
 function showInform({ name, age }) {
@@ -67,19 +67,28 @@ function addUser() {
   event.preventDefault();
   const input = document.querySelectorAll('.inputs');
   const val = Array.from(input).map(el => el.value);
+  // const agePlace = document.querySelector('[placeholder="age"].value');
   const [name, age] = val;
   formAdd.reset();
-  fetch(' https://test-users-api.herokuapp.com/users/', {
-    method: 'POST',
-    body: JSON.stringify({ name, age }),
-    headers: { Accept: 'application/json', 'Content-Type': 'application/json' }
-  })
-  .then(resp => {
-    if (resp.ok) return resp.json();
-    throw new Error(`${resp.statusText}`);
-  })
-    .then(data => showInform(data.data))
-    .catch(err => console.log(err));
+  if (age < '0' || age > '9') {
+    alert('Введите цифры в поле age');
+  } else;
+  {
+    fetch(' https://test-users-api.herokuapp.com/users/', {
+      method: 'POST',
+      body: JSON.stringify({ name, age }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(resp => {
+        if (resp.ok) return resp.json();
+        throw new Error(`${resp.statusText}`);
+      })
+      .then(data => showInform(data.data))
+      .catch(err => alert(`${err}`));
+  }
 }
 
 formAdd.addEventListener('submit', addUser);
@@ -92,12 +101,12 @@ function removeUser() {
   fetch(` https://test-users-api.herokuapp.com/users/${id}`, {
     method: 'DELETE'
   })
-  .then(resp => {
-    if (resp.ok) return resp.json();
-    throw new Error(`${resp.statusText}`);
-  })
+    .then(resp => {
+      if (resp.ok) return resp.json();
+      throw new Error(`${resp.statusText}`);
+    })
     .then(data => showDelete(data.data))
-    .catch(err => console.log(err));
+    .catch(err => alert(`${err}`));
 }
 
 del.addEventListener('click', removeUser);
@@ -110,20 +119,22 @@ function updateUser() {
   let id = input.value;
   let name = inputName.value;
   let age = inputAge.value;
+  if (age < '0' || age > '9') {
+    alert('Введите цифры в поле age');
+  }
   const user = { name, age };
-  console.log(user);
   formChange.reset();
   fetch(` https://test-users-api.herokuapp.com/users/${id}`, {
     method: 'PUT',
     body: JSON.stringify(user),
     headers: { Accept: 'application/json', 'Content-type': 'application/json' }
   })
-  .then(resp => {
-    if (resp.ok) return resp.json();
-    throw new Error(`${resp.statusText}`);
-  })
+    .then(resp => {
+      if (resp.ok) return resp.json();
+      throw new Error(`${resp.statusText}`);
+    })
     .then(data => showUser(data.data))
-    .catch(err => console.log(err));
+    .catch(err => alert(`${err}`));
 }
 
 formChange.addEventListener('submit', updateUser);
