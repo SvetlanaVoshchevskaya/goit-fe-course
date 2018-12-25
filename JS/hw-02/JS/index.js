745435705291fef46d3a751d7002e380401b9d61
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2018-12-25 15:20:28 +0200

    hw-02

--- JS/hw-02/JS/index.js
+++ JS/hw-02/JS/index.js
@@ -5,14 +5,19 @@ let total = 0;
 
 do {
   userInput = prompt('Введите число');
-  if (userInput !== null && userInput !== isNaN) {
+  if (userInput !== null && !isNaN(userInput)) {
     numbers.push(+userInput);
+  } else if (isNaN(userInput)) {
+    alert('Было введено не число, попробуйте еще раз');
   }
 } while (userInput !== null);
 
 for (let value of numbers) {
   total += value;
 }
+
 if (numbers.length !== 0) {
   alert(`Общая сумма чисел равна ${total}`);
 }
+
+console.log(numbers);
