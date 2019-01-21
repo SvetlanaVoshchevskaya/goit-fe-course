4b3701f46887860fea0f45140d643a917b7d16e8
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-01-21 22:38:06 +0200

    hw10

--- JS/hw-10/js/index.js
+++ JS/hw-10/js/index.js
@@ -9,29 +9,33 @@ const add = document.querySelector('.btnAdd');
 const post = document.querySelector('.btnPost');
 const del = document.querySelector('.btnDel');
 const div = document.querySelector('table>tbody');
+const url = 'https://test-users-api.herokuapp.com/users/';
+
+function fetchData(url,method='GET',body,headers={ Accept: 'application/json',
+'Content-Type': 'application/json'}) {
+   return fetch (url,{method,body,headers})
+   }
+
 
 function createView(arr) {
   const result = arr.reduce(
     (acc, el) =>
-    acc +
-    `<tr> <td>${el.id}</td> <td>${el.name}</td><td>${el.age}</td> </tr>`,
+      acc +
+      `<tr> <td>${el.id}</td> <td>${el.name}</td><td>${el.age}</td> </tr>`,
     ''
   );
   div.innerHTML = result;
 }
 
-function showUser({
-  id,
-  name,
-  age
-}) {
+function showUser({ id, name, age }) {
   const data = `<tr> <td>${id}</td> <td>${name}</td> <td>${age}</td> </tr>`;
 
   div.innerHTML = data;
 }
 
+
 function getAllUser() {
-  fetch('https://test-users-api.herokuapp.com/users')
+  fetchData(url)
     .then(resp => {
       if (resp.ok) return resp.json();
       throw new Error(`${resp.statusText}`);
@@ -46,40 +50,32 @@ function getUserById() {
   event.preventDefault();
   let id = input.value;
   input.value = '';
-  fetch(`https://test-users-api.herokuapp.com/users/${id}`)
-    .then(resp => {
-      if (resp.ok) return resp.json();
+  fetchData(url+id)
+    .then(resp => { if (resp.ok) return resp.json();
       throw new Error(`${resp.statusText}`);
     })
     .then(data => showUser(data.data))
     .catch(err => alert(`${err}`));
 }
+get.addEventListener('click', getUserById);
 
-function showInform({
-  name,
-  age
-}) {
+function showInform({ name, age }) {
   const data = alert(
     `Пользователь с именем ${name} и возрастом ${age} добавлен`
   );
   return data;
 }
 
-function showUpdate({
-  id
-}) {
+function showUpdate({ id }) {
   const data = alert(`Пользователь с ID ${id} изменен`);
   return data;
 }
 
-function showDelete({
-  id
-}) {
+function showDelete({ id }) {
   const data = alert(`Пользователь с ID ${id} удален`);
 
   return data;
 }
-get.addEventListener('click', getUserById);
 
 function addUser() {
   event.preventDefault();
@@ -89,18 +85,9 @@ function addUser() {
   formAdd.reset();
   if (age < '0' || age > '9') {
     alert('Введите цифры в поле age');
-  } else; {
-    fetch(' https://test-users-api.herokuapp.com/users/', {
-        method: 'POST',
-        body: JSON.stringify({
-          name,
-          age
-        }),
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
+    return;
+  } else;
+  { fetchData(url,'POST', JSON.stringify({ name, age }))  
       .then(resp => {
         if (resp.ok) return resp.json();
         throw new Error(`${resp.statusText}`);
@@ -120,10 +107,8 @@ function removeUser() {
   const input = document.querySelector('.input_del');
   let id = input.value;
   input.value = '';
-  fetch(` https://test-users-api.herokuapp.com/users/${id}`, {
-      method: 'DELETE'
-    })
-    .then(resp => {
+  fetchData(url+id,'DELETE')
+     .then(resp => {
       if (resp.ok) return resp.json();
       throw new Error(`${resp.statusText}`);
     })
@@ -144,22 +129,14 @@ function updateUser() {
   let id = input.value;
   let name = inputName.value;
   let age = inputAge.value;
+  const user = { name, age };
+  formChange.reset();
   if (age < '0' || age > '9') {
     alert('Введите цифры в поле age');
-  }
-  const user = {
-    name,
-    age
-  };
-  formChange.reset();
-  fetch(` https://test-users-api.herokuapp.com/users/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify(user),
-      headers: {
-        Accept: 'application/json',
-        'Content-type': 'application/json'
-      }
-    })
+    return;
+  } else;
+   {fetchData (url+id,'PUT',JSON.stringify(user) )
+ 
     .then(resp => {
       if (resp.ok) return resp.json();
       throw new Error(`${resp.statusText}`);
@@ -170,6 +147,6 @@ function updateUser() {
       showUpdate(data.data);
     })
     .catch(err => alert(`${err}`));
-}
+}}
 
 formChange.addEventListener('submit', updateUser);
